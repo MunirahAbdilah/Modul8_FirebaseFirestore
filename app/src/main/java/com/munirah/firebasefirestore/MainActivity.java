55da4812a6e9849e2a1d3da1393192a4986e67e8
MunirahAbdilah <munirahabdilah92@gmail.com>
2023-12-16 19:29:12 +0800

    Initial commit

---
+++ app/src/main/java/com/munirah/firebasefirestore/MainActivity.java
@@ -0,0 +1,68 @@
+package com.munirah.firebasefirestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mMainText, mMainText1;
+    private FirebaseFirestore mFirstore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMainText = findViewById(R.id.mainText);
+        mMainText1 = findViewById(R.id.mainText2);
+        Button mSaveBtn = findViewById(R.id.saveBtn);
+        TextView mListText = findViewById(R.id.textView1);
+        ProgressBar progressBar = findViewById(R.id.progress_bar);
+
+        FirebaseApp.initializeApp(this);
+        mFirstore = FirebaseFirestore.getInstance();
+
+        mSaveBtn.setOnClickListener(v ->{
+            String username = mMainText.getText().toString();
+            String stambuk = mMainText1.getText().toString();
+
+            Map<String, String> userMap = new HashMap<>();
+
+            userMap.put("nama", username);
+            userMap.put("stambuk", stambuk);
+
+            progressBar.setVisibility(View.VISIBLE);
+            if(!username.equals("") && !stambuk.equals("")){
+                mFirstore.collection("pengguna").add(userMap).addOnSuccessListener(documentReference -> {
+                    Toast.makeText(MainActivity.this, "Pengguna ditambahkan di Firestore", Toast.LENGTH_SHORT).show();
+                    mMainText.setText("");
+                    mMainText1.setText("");
+                    progressBar.setVisibility(View.GONE);
+                }).addOnFailureListener(e -> {
+                    String error = e.getMessage();
+                    Toast.makeText(MainActivity.this, "Error: "+ error, Toast.LENGTH_SHORT).show();
+                });
+            } else {
+                Toast.makeText(MainActivity.this, "Username dan Stambuk tidak boleh kosong", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mListText.setOnClickListener(v -> {
+            Intent registerIntent = new Intent(MainActivity.this, ListActivity.class);
+            startActivity(registerIntent);
+            Toast.makeText(MainActivity.this, "Buka Activity yang Menampilkan List", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
